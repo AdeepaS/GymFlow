42bd2a436812472e2ae6eb303b1fe56223eb934d
AdeepaS <adeepashashiprabhath@gmail.com>
2026-07-09 08:49:16 +0530

    setup dev environment

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,22 @@ const morgan = require("morgan");
 
 const app = express();
 
+const {
+swaggerUi,
+swaggerSpec
+}=require("./config/swagger");
+
+
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(
+"/api-docs",
+swaggerUi.serve,
+swaggerUi.setup(swaggerSpec)
+);
 
 app.get("/", (req, res) => {
   res.json({
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,21 @@
+require("dotenv").config();
+
 const app = require("./app");
+const redisClient = require("./config/redis");
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+redisClient.connect()
+.then(()=>{
+
+    console.log("Redis connected");
+
+    app.listen(PORT,()=>{
+        console.log(
+            `Server running on ${PORT}`
+        );
+    });
+
 });
